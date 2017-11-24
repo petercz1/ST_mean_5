363fe04c21c8f70f0bd9c35d3bf408f4b4b63bcd
petercz1 <email.peter.carroll@gmail.com>
2017-11-24 10:39:06 +0800

    Prototype designs always work. 		-- Don Vonada

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,3 +1,11 @@
 console.log('loaded frontend app');
 
-angular.module('employees',[]).controller('');
+var thing = angular.module('employees',[])
+thing.controller('do_data', do_data);
+
+
+
+function do_data($scope, $http) {
+    console.log('doing data stuff');
+
+}
