e22ba94e396f801a5fa657a78df37b86d7de852d
petercz1 <email.peter.carroll@gmail.com>
2017-11-24 10:40:12 +0800

    Boys are beyond the range of anybody's sure understanding, at least when they are between the ages of 18 months and 90 years. 		-- James Thurber

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,11 +1,26 @@
 console.log('loaded frontend app');
 
-var thing = angular.module('employees',[])
-thing.controller('do_data', do_data);
+angular.module('employees',[]).controller('do_data', do_data);
 
 
 
 function do_data($scope, $http) {
     console.log('doing data stuff');
+
+    $scope.read = function () {
+        console.log('getting all data');
+    }
+
+    $scope.create = function () {
+        console.log('getting all data');
+    }
+
+    $scope.update = function () {
+        console.log('getting all data');
+    }
+
+    $scope.delete = function () {
+        console.log('getting all data');
+    }
 
 }
